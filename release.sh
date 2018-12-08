d2b54ea70d6c86ec25fe97f784c952b17a46555d
bebauer <bebauer@users.noreply.github.com>
2018-12-08 14:25:02 +0100

    update release script

--- release.sh
+++ release.sh
@@ -2,8 +2,29 @@
 
 set -e
 
+increment=$(echo ${1-minor} | awk '{print toupper($0)}')
+
+case "$increment" in
+"PATCH")
+    param="incrementPatch"
+    ;;
+"MINOR")
+    param="incrementMinor"
+    ;;
+"MAJOR")
+    param="incrementMajor"
+    ;;
+"PRERELEASE")
+    param="incrementPrerelease"
+    ;;
+*)
+    echo "invalid value for increment: ${increment}"
+    exit 1
+    ;;
+esac
+
 # make release
-./gradlew release
+./gradlew release -Prelease.versionIncrementer=${param}
 
 # upload artifacts to Bintray
 ./gradlew bintrayUpload
