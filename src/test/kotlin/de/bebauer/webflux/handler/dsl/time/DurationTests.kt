b4a4bdc38a8bfb06ddffd686866d1a97b6fb2711
Benjamin Bauer <benjamin.bauer@codecentric.de>
2018-12-21 15:17:44 +0100

    duration tests

--- src/test/kotlin/de/bebauer/webflux/handler/dsl/time/DurationTests.kt
+++ src/test/kotlin/de/bebauer/webflux/handler/dsl/time/DurationTests.kt
@@ -1,40 +1,31 @@
 package de.bebauer.webflux.handler.dsl.time
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.Arguments
-import org.junit.jupiter.params.provider.Arguments.arguments
-import org.junit.jupiter.params.provider.MethodSource
+import io.kotlintest.data.forall
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.StringSpec
+import io.kotlintest.tables.row
 import java.time.Duration
 import java.time.temporal.ChronoUnit
-import java.util.stream.Stream
 
-class DurationTests {
-
-    companion object {
-        @Suppress("unused")
-        @JvmStatic
-        fun testArgs(): Stream<Arguments> = Stream.of(
-            arguments(1.day, Duration.ofDays(1)),
-            arguments(10.days, Duration.ofDays(10)),
-            arguments(1.hour, Duration.ofHours(1)),
-            arguments(10.hours, Duration.ofHours(10)),
-            arguments(1.minute, Duration.ofMinutes(1)),
-            arguments(10.minutes, Duration.ofMinutes(10)),
-            arguments(1.second, Duration.ofSeconds(1)),
-            arguments(10.seconds, Duration.ofSeconds(10)),
-            arguments(1.millisecond, Duration.ofMillis(1)),
-            arguments(10.milliseconds, Duration.ofMillis(10)),
-            arguments(1.microsecond, Duration.of(1, ChronoUnit.MICROS)),
-            arguments(10.microseconds, Duration.of(10, ChronoUnit.MICROS)),
-            arguments(1.nanosecond, Duration.ofNanos(1)),
-            arguments(10.nanoseconds, Duration.ofNanos(10))
-        )
+class DurationTests : StringSpec({
+    "test duration creation" {
+        forall(
+            row(1.day, Duration.ofDays(1)),
+            row(10.days, Duration.ofDays(10)),
+            row(1.hour, Duration.ofHours(1)),
+            row(10.hours, Duration.ofHours(10)),
+            row(1.minute, Duration.ofMinutes(1)),
+            row(10.minutes, Duration.ofMinutes(10)),
+            row(1.second, Duration.ofSeconds(1)),
+            row(10.seconds, Duration.ofSeconds(10)),
+            row(1.millisecond, Duration.ofMillis(1)),
+            row(10.milliseconds, Duration.ofMillis(10)),
+            row(1.microsecond, Duration.of(1, ChronoUnit.MICROS)),
+            row(10.microseconds, Duration.of(10, ChronoUnit.MICROS)),
+            row(1.nanosecond, Duration.ofNanos(1)),
+            row(10.nanoseconds, Duration.ofNanos(10))
+        ) { actual, expected ->
+            actual shouldBe expected
+        }
     }
-
-    @ParameterizedTest
-    @MethodSource("testArgs")
-    fun `test duration creation`(actual: Duration, expected: Duration) {
-        assertThat(actual).isEqualTo(expected)
-    }
-}
+})
