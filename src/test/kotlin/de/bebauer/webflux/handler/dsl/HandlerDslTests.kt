8f6bfba4a3be841ca6c5aa9438407b1fc12af0b7
Benjamin Bauer <benjamin.bauer@codecentric.de>
2018-12-21 13:37:12 +0100

    handler DSL tests

--- src/test/kotlin/de/bebauer/webflux/handler/dsl/HandlerDslTests.kt
+++ src/test/kotlin/de/bebauer/webflux/handler/dsl/HandlerDslTests.kt
@@ -1,7 +1,9 @@
 package de.bebauer.webflux.handler.dsl
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import io.kotlintest.data.forall
+import io.kotlintest.shouldBe
+import io.kotlintest.specs.WordSpec
+import io.kotlintest.tables.row
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.ValueSource
 import org.springframework.http.HttpStatus
@@ -10,130 +12,112 @@ import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
 
-class HandlerDslTests {
+class HandlerDslTests : WordSpec({
 
-    @Test
-    fun `fail without complete`() {
-        runHandlerTest(
-            handler { },
-            { expectStatus().is5xxServerError })
+    "handler DSL" should {
+        "fail without a complete" {
+            runHandlerTest(
+                handler { },
+                { expectStatus().is5xxServerError })
+        }
+
+        "fail with multiple completes" {
+            runHandlerTest(handler {
+                complete("xxx")
+                complete("abc")
+            }, { expectStatus().is5xxServerError })
+        }
+
+        "allow completions inside if - else" {
+            forall(row("a"), row("b")) { value ->
+                runHandlerTest(
+                    handler {
+                        if (value == "a") complete("a") else complete("b")
+                    },
+                    {
+                        expectStatus().isOk
+                            .expectBody(String::class.java)
+                            .returnResult().responseBody shouldBe value
+                    })
+            }
+        }
+
+        "execute a sub DSL which produces a correct response" {
+            runHandlerTest(
+                handler {
+                    val result = execute {
+                        complete("Test")
+                    }
+
+                    complete(result.flatMap { ServerResponse.from(it).body(fromObject("Yay!")) })
+                },
+                {
+                    expectStatus().isOk
+                        .expectBody(String::class.java)
+                        .returnResult().responseBody shouldBe "Yay!"
+                })
+        }
+
+        "execute a sub DSL which produces an error" {
+            runHandlerTest(
+                handler {
+                    val result = execute {
+                        failWith("Test")
+                    }
+
+                    complete(result.onErrorResume { ServerResponse.ok().body(fromObject("Nay!")) })
+                },
+                {
+                    expectStatus().isOk
+                        .expectBody(String::class.java)
+                        .returnResult().responseBody shouldBe "Nay!"
+                })
+        }
+
+        "complete with the first non empty completion in a chain (last)" {
+            runHandlerTest(
+                handler {
+                    complete(Mono.empty()) or complete(Mono.empty<String>()) or complete(HttpStatus.NOT_FOUND, "Test")
+                },
+                {
+                    expectStatus().isNotFound.expectBody(String::class.java).returnResult()
+                        .responseBody shouldBe "Test"
+                })
+        }
+
+        "complete with the first non empty completion in a chain (first)" {
+            runHandlerTest(
+                handler {
+                    complete("1") or complete(HttpStatus.NOT_FOUND, "1")
+                },
+                {
+                    expectStatus().isOk.expectBody(String::class.java).returnResult()
+                        .responseBody shouldBe "1"
+                })
+        }
+
+        "allow failWith in a completions chain" {
+            runHandlerTest(
+                handler {
+                    complete(Mono.empty()) or failWith("Error!")
+                },
+                {
+                    expectStatus().is5xxServerError
+                })
+        }
     }
 
-    @Test
-    fun `fail with multiple complete`() {
-        runHandlerTest(handler {
-            complete("xxx")
-            complete("abc")
-        }, { expectStatus().is5xxServerError })
+    "failWith" should {
+        "cause a status code response when called with a 'ResponseStatusException'" {
+            runHandlerTest(
+                handler { failWith(ResponseStatusException(HttpStatus.BAD_REQUEST)) },
+                { expectStatus().isBadRequest })
+        }
+
+        "cause an internal server error if called with a 'String'" {
+            runHandlerTest(
+                handler { failWith("Failure") },
+                { expectStatus().is5xxServerError })
+        }
     }
-
-    @ParameterizedTest
-    @ValueSource(strings = ["a", "b"])
-    fun `if - else complete`(value: String) {
-        runHandlerTest(
-            handler {
-                if (value == "a") complete("a") else complete("b")
-            },
-            {
-                expectStatus().isOk.expectBody(String::class.java).returnResult()
-                    .apply { assertThat(responseBody).isEqualTo(value) }
-            })
-    }
-
-    @Test
-    fun `explicit failure with exception`() {
-        runHandlerTest(
-            handler { failWith(ResponseStatusException(HttpStatus.BAD_REQUEST)) },
-            { expectStatus().isBadRequest })
-    }
-
-    @Test
-    fun `explicit failure with error message`() {
-        runHandlerTest(
-            handler { failWith("Failure") },
-            { expectStatus().is5xxServerError })
-    }
-
-    @Test
-    fun `execute sub DSL ok`() {
-        runHandlerTest(
-            handler {
-                val result = execute {
-                    complete("Test")
-                }
-
-                complete(result.flatMap { ServerResponse.from(it).body(fromObject("Yay!")) })
-            },
-            {
-                expectStatus().isOk.expectBody(String::class.java).returnResult()
-                    .apply { assertThat(responseBody).isEqualTo("Yay!") }
-            })
-    }
-
-    @Test
-    fun `execute sub DSL failure`() {
-        runHandlerTest(
-            handler {
-                val result = execute {
-                    failWith("Test")
-                }
-
-                complete(result.onErrorResume { ServerResponse.ok().body(fromObject("Nay!")) })
-            },
-            {
-                expectStatus().isOk.expectBody(String::class.java).returnResult()
-                    .apply { assertThat(responseBody).isEqualTo("Nay!") }
-            })
-    }
-
-    @Test
-    fun `complete with Either`() {
-        runHandlerTest(
-            handler {
-                val result = execute {
-                    complete("Test")
-                }
-
-                complete(result)
-            },
-            {
-                expectStatus().isOk.expectBody(String::class.java).returnResult()
-                    .apply { assertThat(responseBody).isEqualTo("Test") }
-            })
-    }
-
-    @Test
-    fun `alternative completions last`() {
-        runHandlerTest(
-            handler {
-                complete(Mono.empty()) or complete(Mono.empty<String>()) or complete(HttpStatus.NOT_FOUND, "Test")
-            },
-            {
-                expectStatus().isNotFound.expectBody(String::class.java).returnResult()
-                    .apply { assertThat(responseBody).isEqualTo("Test") }
-            })
-    }
-
-    @Test
-    fun `alternative completions first`() {
-        runHandlerTest(
-            handler {
-                complete("1") or complete(HttpStatus.NOT_FOUND, "1")
-            },
-            {
-                expectStatus().isOk.expectBody(String::class.java).returnResult()
-                    .apply { assertThat(responseBody).isEqualTo("1") }
-            })
-    }
-
-    @Test
-    fun `alternative failWith`() {
-        runHandlerTest(
-            handler {
-                complete(Mono.empty()) or failWith("Error!")
-            },
-            {
-                expectStatus().is5xxServerError
-            })
-    }
-}
+})
